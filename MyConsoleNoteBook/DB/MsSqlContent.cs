11d16fca5da8d476c1ac07880bb6a46a8df566c2
frakiec89 <45938852+frakiec89@users.noreply.github.com>
2022-03-02 10:49:42 +0400

    ewew

---
+++ MyConsoleNoteBook/DB/MsSqlContent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsoleNoteBook.DB
+{
+    public class MsSqlContent
+    {
+        static  string server = "192.168.10.134";
+        static string database = "Ahtyamov_for_IS-20_02_notebook";
+        static string user = "stud";
+        static string password = "stud";
+
+
+        private string GetConnectionString (string server , string database, string user, string password)
+        {
+            return $"Server = {server}; Database={database};User Id = {user}; Password={password}";
+        }
+    }
+}
