a947c6aa017476830d1310e1acda0b953bc45862
frakiec89 <45938852+frakiec89@users.noreply.github.com>
2022-03-02 08:47:24 +0400

    Добавьте файлы проекта.

---
+++ MyConsoleNoteBook/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyConsoleNoteBook
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Моя записная книжка");
+            while(true)
+            {
+                MenuPrint();
+
+                switch(Console.ReadLine().ToLower().TrimStart().TrimEnd())
+                {
+                    case "exit": return;
+                    default: Console.WriteLine("команда не опознана"); break;
+                }
+
+            }
+        }
+
+        private static void MenuPrint() // вывод на  экран  список  доступных команд
+        {
+            string exit = "exit"; // команды
+            Console.WriteLine($"Для выхода из программы введите \"{exit}\"");
+        }
+    }
+}
